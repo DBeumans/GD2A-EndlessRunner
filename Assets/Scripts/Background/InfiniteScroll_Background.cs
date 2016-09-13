7bb61a075f4cdc2b3db61a438cbc04113dfcfe53
DBeumans <buncho.gamingnetwork@gmail.com>
2016-09-13 09:39:33 +0200

    Added Infinite Background Scrolling, added some maps

---
+++ Assets/Scripts/Background/InfiniteScroll_Background.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfiniteScroll_Background : MonoBehaviour {
+
+    [SerializeField]
+    float ScrollSpeed = 0.1f;
+
+    void Update()
+    {
+        Vector2 offset = new Vector2(Time.time * -ScrollSpeed, 0);
+
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
