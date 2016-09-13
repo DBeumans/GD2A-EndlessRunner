fb9d2c5426dd89ecfce8cb4061b9b444e3c26a88
DBeumans <buncho.gamingnetwork@gmail.com>
2016-09-13 22:34:39 +0200

    Added spawning for the houses and added movement to it

---
+++ Assets/Scripts/House/House_Movement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class House_Movement : MonoBehaviour {
+
+    [SerializeField]
+    float MovementSpeed = 1;
+
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        transform.Translate(Vector2.right * MovementSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Spawning/House_Spawner.cs
+++ Assets/Scripts/Spawning/House_Spawner.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class House_Spawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [SerializeField]
+    GameObject House;                
+    float spawnTime = .3f;   
+    [SerializeField]        
+    Transform[] spawnPoints;         
+
+
+    void Start()
+    {
+        
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+       
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        
+        Instantiate(House, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+
+
 }
