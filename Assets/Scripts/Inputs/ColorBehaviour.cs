834472311e392a0c5c2092966a35c36aac28d993
Rens <r.walstijn@live.nl>
2016-09-22 13:02:27 +0200

    Added ColorBehaviour

---
+++ Assets/Scripts/Inputs/ColorBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorBehaviour : MonoBehaviour {
+	
+	SpriteRenderer _color;
+
+	void Awake () {
+		_color = GetComponent<SpriteRenderer>();
+		_color.color = new Color(0, 0, 0, 1);
+	}
+
+	void Update () {
+
+	}
+
+    void GetColor() {
+
+    }
+
+	void SetColor(string newColor) {
+		
+	}
+}
