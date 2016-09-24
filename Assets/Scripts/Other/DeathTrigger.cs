3414f6bcc7928029b37ec4ba47c8aa28b6182a00
DBeumans <buncho.gamingnetwork@gmail.com>
2016-09-24 17:16:12 +0200

    Added Death Trigger Script, destroying everything if it have a rigidbody componend on it

---
+++ Assets/Scripts/Other/DeathTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer != LayerMask.NameToLayer("Death"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
