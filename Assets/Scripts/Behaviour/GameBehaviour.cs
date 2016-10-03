0f184f8761148210a4959483d654ada3a2b16784
DBeumans <buncho.gamingnetwork@gmail.com>
2016-10-03 07:39:42 +0200

    Added one time house spawner, changed groundcheck position voor player

--- Assets/Scripts/Behaviour/GameBehaviour.cs
+++ Assets/Scripts/Behaviour/GameBehaviour.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class GameBehaviour : MonoBehaviour {
 
+    //If player is at the mainmenu Game_Started boolean is set to false, 
+    //if player pressed "Start" Game_Started is set to True, object spawners wait for this boolean to act.
+    public static bool Game_Started = false;
 	// Use this for initialization
 	void Start () {
 	
---
+++ Assets/Scripts/Spawning/House_Spawner_One_Time.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class House_Spawner_One_Time : SpawningBehaviour {
+
+    // storage voor houses
+    // random house pakken 
+    // spawnen
+
+    [SerializeField]
+    GameObject[] Houses;
+
+    [SerializeField]
+    Transform[] spawnPoint_1;
+    [SerializeField]
+    Transform[] spawnPoint_2;
+
+    void Start()
+    {
+        ObjectSpawner(Houses, spawnPoint_1);
+        ObjectSpawner(Houses, spawnPoint_2);
+    }
+}
