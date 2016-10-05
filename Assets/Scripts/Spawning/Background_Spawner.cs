919123b839f068fc13fdd74a2d7abcdf5c06b561
DBeumans <buncho.gamingnetwork@gmail.com>
2016-10-05 09:11:13 +0200

    commit danilo

--- Assets/Scripts/Spawning/Background_Spawner.cs
+++ Assets/Scripts/Spawning/Background_Spawner.cs
@@ -6,14 +6,15 @@ public class Background_Spawner : SpawningBehaviour {
     //Object to spawn , its a array so you can spawn different objects of the same type.
     [SerializeField]
     GameObject[] Backgrounds;
-    //Spawn timer, change the spawn timer to spawn objects faster or slower.
-    [SerializeField]
-    float spawnTime = 1f;
     //Spawnpoints, where the objects needs to spawn.
     [SerializeField]
     Transform spawnPoints;
     public bool _spawnable = false;
 
+    void Start()
+    {
+        Spawn();
+    }
     void Update()
     {
         if(_spawnable)
--- Assets/Scripts/Spawning/CheckSpawning.cs
+++ Assets/Scripts/Spawning/CheckSpawning.cs
@@ -5,24 +5,33 @@ public class CheckSpawning : MonoBehaviour {
 
     House_Spawner _house_Spawner;
     Background_Spawner _background_Spawner;
+    string _gameObject_Name;
 
     void Start()
     {
         _house_Spawner = GameObject.FindObjectOfType<House_Spawner>();
         _background_Spawner = GameObject.FindObjectOfType<Background_Spawner>();
+
+        _gameObject_Name = this.gameObject.tag;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Spawn_House")
+        switch(_gameObject_Name)
         {
-            _house_Spawner._spawnable = true;
-            
-        }
-        if(other.gameObject.tag == "Spawn_Background")
-        {
-            _background_Spawner._spawnable = true;
+            case "Background_Trigger":
+                if (other.gameObject.tag == "Spawn_Background")
+                {
+                    _background_Spawner._spawnable = true;
+                }
+                break;
+
+            case "House_Trigger":
+                if (other.gameObject.tag == "Spawn_House")
+                {
+                    _house_Spawner._spawnable = true;
+                }
+                break;
         }
     }
-
 }
