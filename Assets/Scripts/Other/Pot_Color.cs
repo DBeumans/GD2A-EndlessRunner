4d3e487f279348c95d81965f3e277869e5fab3a9
Rens <r.walstijn@live.nl>
2016-10-07 11:51:01 +0200

    new push

--- Assets/Scripts/Other/Pot_Color.cs
+++ Assets/Scripts/Other/Pot_Color.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class Pot_Color : ColorBehaviour {
     
     private void Stard() {
-        //sets the color of a pot
-        base.setColor("#", "#");
+        base.setColor("","");
     }
 }
